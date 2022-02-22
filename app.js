c85339517f9ed8466b0e77984b630c031a15ce32
Priyanshu <princepriyanshu.vaishnav@gmail.com>
2022-02-22 13:09:04 +0530

    Deleted useless files

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express=require("express");
 const app=express();
 
+app.use(express.static("public"));
+
+app.get("/",function(req,res)
+{
+  res.sendFile(__dirname+"/index.html");
+});
+
 app.listen(3000,function()
 {
-  console.log("Hello and Welcome to the server").
-})
+  console.log("Hello and Welcome to the server");
+});
